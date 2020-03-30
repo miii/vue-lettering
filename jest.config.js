bd6152611e75e8b31c196d088e9e2b456540b8d7
Jacob Andersson <jacob.m.andersson@gmail.com>
2020-03-30 17:55:15 +0200

    test: 💍 Add nuxt module tests

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript'
+  preset: '@vue/cli-plugin-unit-jest/presets/typescript',
+  transform: {
+    '^.+\\.js$': 'babel-jest'
+  }
 }
